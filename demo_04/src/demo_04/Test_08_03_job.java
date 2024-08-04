251f62807bb3cffe5ead6b34f99647b4867d8fdc
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-08-04 19:48:13 +0900

    Test_08_03_job

--- demo_04/src/demo_04/Test_08_03_job.java
+++ demo_04/src/demo_04/Test_08_03_job.java
@@ -6,7 +6,7 @@ public class Test_08_03_job
 	{
 		Calc03 calc = new Calc03( "Poly-morphism" );
 		calc.process( () -> {
-			System.out.println( "関心事を記述" );
+			System.out.println( "クラスパスを無視" );
 
 			Interface save = Calc03.create();
 				save.execute();
