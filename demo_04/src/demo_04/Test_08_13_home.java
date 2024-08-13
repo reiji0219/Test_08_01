ab569b70192e28f54c3181da8271283399a461ce
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-08-13 23:44:28 +0900

    Test_08_13_home

---
+++ demo_04/src/demo_04/Test_08_13_home.java
@@ -0,0 +1,24 @@
+package demo_04;
+
+public class Test_08_13_home 
+{
+	public static void main( String[] args )
+	{
+		Calc13 calc = new Calc13();
+		calc.process( () -> {
+			System.out.println( "関心事を記述" );
+			
+			Interface save = Calc13.create();
+				save.execute();
+			
+		});
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
