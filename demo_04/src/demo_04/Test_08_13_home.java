5c0ef881b4ee3f39b8ffb9781399857a1b48fe26
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-08-13 23:59:12 +0900

    Test_08_13_home

--- demo_04/src/demo_04/Test_08_13_home.java
+++ demo_04/src/demo_04/Test_08_13_home.java
@@ -4,7 +4,7 @@ public class Test_08_13_home
 {
 	public static void main( String[] args )
 	{
-		Calc13 calc = new Calc13();
+		Calc13 calc = new Calc13( "Poly-morphism" );
 		calc.process( () -> {
 			System.out.println( "関心事を記述" );
 			
