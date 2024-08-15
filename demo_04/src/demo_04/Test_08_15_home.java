90a4178ae378d33c75922ef941a229c6c1823ae3
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-08-15 23:59:15 +0900

    test

--- demo_04/src/demo_04/Test_08_15_home.java
+++ demo_04/src/demo_04/Test_08_15_home.java
@@ -1,5 +1,15 @@
 package demo_04;
 
-public class Test_08_15_home {
-
+public class Test_08_15_home 
+{
+	public static void main( String[] args ) 
+	{
+		Item15.save( item -> {
+			item.id( "")
+				
+		});
+		
+		
+		
+	}
 }
